91a4224c6fc613af3e54a73eb4440c9147da114c
Emanuel <Emanuel@DESKTOP-CMJTQJI>
2023-12-08 19:59:34 -0600

    Modificacion Constantes

--- src/java/Utilidades/Constantes.java
+++ src/java/Utilidades/Constantes.java
@@ -1,12 +1,8 @@
 
 package Utilidades;
 
-import Servlets.Maestros.CrearMaestro_Srv;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 public class Constantes {
